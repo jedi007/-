7da05ee621d8b278304d2884237a672854b8d152
jedi007 <360751194@qq.com>
2020-02-01 23:30:01 +0800

    调通登陆接口

--- 都信/Models/httpManager.swift
+++ 都信/Models/httpManager.swift
@@ -17,6 +17,75 @@ class httpManager {
         print("httpManager inited")
     }
     
+    
+    func login( telephone:String, password:String,  failed:@escaping (_ errorCode:Int)->Void, success:@escaping ()->Void )
+    {
+        let session = URLSession(configuration: .default)
+        // 设置URL(该地址不可用，写你自己的服务器地址)
+        let url = "\(baseUrl)/login"
+        var request = URLRequest(url: URL(string: url)!, timeoutInterval: 5)
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        // 设置要post的内容，字典格式
+        let postData = ["telephone":"\(telephone)", "password":"\(password)"]
+        let postString = postData.compactMap({ (key, value) -> String in
+            return "\(key)=\(value)"
+        }).joined(separator: "&")
+        request.httpBody = postString.data(using: .utf8)
+        // 后面不解释了，和GET的注释一样
+        let task = session.dataTask(with: request) {(data, response, error) in
+            if error != nil
+            {
+                print("error : \(error!.localizedDescription)")
+                failed(-201)
+                return;
+            }
+            print(" in response ")
+            do{
+                //              将二进制数据转换为字典对象
+                if let jsonObj:NSDictionary = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions()) as? NSDictionary
+                {
+                    print(jsonObj)
+                    print(type(of: jsonObj["user"]))
+//                    let test = 
+//                    print( jsonObj["user"] )
+//                    if let jsonObj_user:NSDictionary = try JSONSerialization.jsonObject(with: jsonObj["user"], options: JSONSerialization.ReadingOptions()) as? NSDictionary
+//                    {
+//                        print("jsonObj_user: \(jsonObj_user)")
+//                    }
+                    if let result_code = jsonObj["result"] as? Int{
+                        print( type(of: result_code) )
+                        
+                        if result_code == 0 {
+                            //主线程
+                            DispatchQueue.main.async{
+                                success()
+                            }
+                        }
+                        else
+                        {
+                            failed(result_code)
+                        }
+                    }
+                    else
+                    {
+                        failed(-202)
+                    }
+                }
+            } catch{
+                print("Error.")
+                failed(-203)
+                DispatchQueue.main.async{
+                    
+                }
+                
+            }
+        }
+        // 启动任务
+        task.resume()
+    }
+    
+    
     func regist( registeInfoDic:Dictionary<String,String>, failed:@escaping (_ errorCode:Int)->Void, success:@escaping ()->Void ) {
         let session = URLSession(configuration: .default)
         // 设置URL(该地址不可用，写你自己的服务器地址)
--- 都信/ViewController.swift
+++ 都信/ViewController.swift
@@ -14,9 +14,38 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func login(_ sender: UIButton) {
+        print("点击了 登陆")
+        
+        httpManager.shared.login(telephone: "13568991512", password: "qqqqqqqq".md5,failed:{(errorCode:Int) in
+            print("get failed info at out, errorCode: \(errorCode)")
+            
+            if errorCode == -1
+            {
+                DispatchQueue.main.async{
+                    self.alertDialog(title: "登陆失败", message: "该账号已经注册", actionText: "知道了")
+                }
+            }
+            else
+            {
+                DispatchQueue.main.async{
+                    self.alertDialog(title: "登陆失败", message: "错误码： \(errorCode)", actionText: "知道了")
+                }
+            }
+        } ){
+            print("get success info at out")
+            self.alertDialog(title: "登陆成功", message: "登陆成功", actionText: "OK", actionHandler: nil)
+        }
+        
+        
+    }
+    
+    
 
     @IBAction func register(_ sender: UIButton) {
-        print("注册被点击了")
+        print("点击了 注册")
         
         let ndate = DateTools.shared.stringConvertDate(string: "2019-05-13 19:58:44")
         
@@ -24,6 +53,16 @@ class ViewController: UIViewController {
         
         print("日期转换测试: "+strDate)
 
+    }
+    
+    
+    func alertDialog(title:String, message:String, actionText:String, actionHandler:((UIAlertAction) -> Void)? = nil ) ->Void {
+        let alertController = UIAlertController(title: title, message: message,preferredStyle: .alert)
+        let Action1 = UIAlertAction(title: actionText, style: .destructive, handler: actionHandler)
+        //let Action2 = UIAlertAction(title: "取消", style: .cancel, handler: nil)
+        alertController.addAction(Action1)
+        //alertController.addAction(Action2)
+        self.present(alertController, animated: true, completion: nil)
     }
     
 }
