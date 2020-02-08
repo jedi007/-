b22a823250574f5a4c7ed4e27c5eb072c342ef0b
jedi007 <360751194@qq.com>
2020-02-08 15:27:49 +0800

    完成发现页面的UI

---
+++ 都信/discoverViewController.swift
@@ -0,0 +1,14 @@
+//
+//  discoverViewController.swift
+//  都信
+//
+//  Created by 李杰 on 2020/2/8.
+//  Copyright © 2020 李杰. All rights reserved.
+//
+
+import UIKit
+
+class discoverViewController: UIViewController {
+    
+    
+}
