019f661dd4cae7ed357be02e70af2a7b32953634
jedi007 <360751194@qq.com>
2020-04-11 23:12:25 +0800

    实现圆角矩形的自绘

--- code/Models/ChatBubble.swift
+++ code/Models/ChatBubble.swift
@@ -39,25 +39,42 @@ class ChatBubbleView: UIView {
 
         let path = UIBezierPath()
         
-        UIColor.lightGray.setStroke()
-        path.lineWidth = 5
-        path.stroke()
+        let radius:CGFloat = 5
         
-        path.move(to: CGPoint(x:6, y:14))
+        //画圆角,clockwise: true 顺时针方向
+        path.addArc(withCenter: CGPoint(x: 11, y: radius), radius: radius, startAngle: CGFloat(Double.pi)*1.5, endAngle: CGFloat(Double.pi), clockwise: false)
+        
+        //画气泡尖
+        path.addLine(to: CGPoint(x:6, y:14))
         path.addLine(to: CGPoint(x:0, y:20))
         path.addLine(to: CGPoint(x:6, y:26))
-        path.addLine(to: CGPoint(x:6, y:viewHeight!))
-        path.addLine(to: CGPoint(x:viewWidth!, y:viewHeight!))
-        path.addLine(to: CGPoint(x:viewWidth!, y:0))
-        path.addLine(to: CGPoint(x:6, y:0))
-        path.close()  //闭合路径，把起始点和终点连接起来
+        
+        path.addLine(to: CGPoint(x:6, y:viewHeight!-radius))
+        path.addArc(withCenter: CGPoint(x: 6+radius, y: viewHeight!-radius), radius: radius, startAngle: CGFloat(Double.pi), endAngle: CGFloat(Double.pi)*0.5, clockwise: false)
+
+        path.addLine(to: CGPoint(x:viewWidth!-radius, y:viewHeight!))
+        path.addArc(withCenter: CGPoint(x: viewWidth!-radius, y: viewHeight!-radius), radius: radius, startAngle: CGFloat(Double.pi)*0.5, endAngle: 0, clockwise: false)
+
+        path.addLine(to: CGPoint(x:viewWidth!, y:radius))
+        path.addArc(withCenter: CGPoint(x: viewWidth!-radius, y: radius), radius: radius, startAngle: 0, endAngle: CGFloat(Double.pi)*1.5, clockwise: false)
+
+        path.addLine(to: CGPoint(x:6+radius, y:0))
+        
+//        UIColor.red.setFill()
+//        UIColor.black.setStroke()
+//
+//        path.fill()
+//        path.stroke()
+        
+        //path.close()  //闭合路径，把起始点和终点连接起来
+        
 
         let layer = CAShapeLayer()
         layer.path = path.cgPath
         
         self.layer.mask = layer
         
-        let showlabel = UILabel(frame: CGRect(x: 11, y: 5, width: viewWidth!-16, height: viewHeight!-10))
+        let showlabel = UILabel(frame: CGRect(x: 6+radius, y: radius, width: viewWidth!-16, height: viewHeight!-10))
         showlabel.text = messageStr
         
         //两行代码实现label自动换行
